97f1c808a9a48cbd829c08f63a92e34b3a8a53d2
Milad Rastian <miladrastian@gmail.com>
2022-05-27 20:54:40 +0200

    update settings

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Arctic.Base.MixProject do
   def project do
     [
       app: :arctic_base,
-      name: "Arctic.Base"
+      name: "Arctic.Base",
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
@@ -21,9 +21,6 @@ defmodule Arctic.Base.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    []
   end
 end
